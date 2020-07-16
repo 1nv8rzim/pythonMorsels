618e1a9ec620ced0694337939eb4cc98549ab910
Maxwell <msf9542@g.rit.edu>
2020-07-16 18:27:19 -0400

    Bonus 1 and 2

--- fix_csv/fix_csv.py
+++ fix_csv/fix_csv.py
@@ -1,10 +1,22 @@
-import csv
-import sys
+parser = ArgumentParser()
+parser.add_argument('old_filename')
+parser.add_argument('new_filename')
+parser.add_argument('--in-delimiter', dest='delim')
+parser.add_argument('--in-quote', dest='quote')
+args = parser.parse_args()
 
+with open(args.old_filename, newline='') as old_file:
+    arguments = {}
+    if args.delim:
+        arguments['delimiter'] = args.delim
+    if args.quote:
+        arguments['quotechar'] = args.quote
+    if not args.delim and not args.quote:
+        arguments['dialect'] = csv.Sniffer().sniff(old_file.read())
+        old_file.seek(0)
+    reader = csv.reader(old_file, **arguments)
+    rows = list(reader)
 
-old, new = sys.argv[1:]
-
-with open(old, newline='') as old_csv:
-    reader = csv.reader(old_csv, delimiter='|')
-    with open(new, mode='wt', newline='') as new_csv:
-        csv.writer(new_csv).writerows(reader)
+with open(args.new_filename, mode='wt', newline='') as new_file:
+    writer = csv.writer(new_file)
+    writer.writerows(rows)
