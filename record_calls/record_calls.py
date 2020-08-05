e4d38a93e2affb8c74f078ced44163872dd99771
Maxwell <msf9542@g.rit.edu>
2020-08-04 21:28:55 -0400

    cannot get bonus 1 to work

--- record_calls/record_calls.py
+++ record_calls/record_calls.py
@@ -1,8 +1,19 @@
+from functools import wraps
+
+
 class record_calls:
+
     def __call__(self, *args, **kwargs):
         self.call_count += 1
         return self.function(*args, **kwargs)
 
     def __init__(self, function):
-        self.function = function
+        self.function = record_calls.test(function)
         self.call_count = 0
+
+    @staticmethod
+    def test(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            return function(*args, **kwargs)
+        return wrapper
