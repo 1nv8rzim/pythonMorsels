35e4a543005be90a3a833af3e141ab5a76d6aa02
Maxwell <msf9542@g.rit.edu>
2020-08-04 21:51:05 -0400

    finished all bonuses

--- record_calls/record_calls.py
+++ record_calls/record_calls.py
@@ -1,19 +1,29 @@
 from functools import wraps
 
+NO_RETURN = object()
 
-class record_calls:
 
-    def __call__(self, *args, **kwargs):
-        self.call_count += 1
-        return self.function(*args, **kwargs)
+class Call:
+    def __init__(self, args, kwargs, return_value, exception):
+        self.args = args
+        self.kwargs = kwargs
+        self.return_value = return_value
+        self.exception = exception
 
-    def __init__(self, function):
-        self.function = record_calls.test(function)
-        self.call_count = 0
 
-    @staticmethod
-    def test(function):
-        @wraps(function)
-        def wrapper(*args, **kwargs):
-            return function(*args, **kwargs)
-        return wrapper
+def record_calls(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        wrapper.call_count += 1
+        try:
+            return_value = function(*args, **kwargs)
+        except Exception as e:
+            wrapper.calls.append(Call(args, kwargs, NO_RETURN, e))
+            raise(e)
+        wrapper.calls.append(Call(args, kwargs, return_value, None))
+        return return_value
+
+    wrapper.call_count = 0
+    wrapper.calls = []
+
+    return wrapper
