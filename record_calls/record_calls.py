4cc5948eaf95d45fcd8c6890eaa09f7e1f6c26fe
Maxwell <msf9542@g.rit.edu>
2020-08-04 20:41:15 -0400

    minor changes for bug fixing

--- record_calls/record_calls.py
+++ record_calls/record_calls.py
@@ -1,7 +1,7 @@
 class record_calls:
     def __call__(self, *args, **kwargs):
         self.call_count += 1
-        return self.function(*args, *kwargs)
+        return self.function(*args, **kwargs)
 
     def __init__(self, function):
         self.function = function
