2c5db19405421d1a0ce14e5647c6be4eeedb27f7
Maxwell <msf9542@g.rit.edu>
2020-08-12 19:44:28 -0400

    base, and bonus 1 finished. WOrking on bonus 2

---
+++ deep_flatten/deep_flatten.py
@@ -0,0 +1,12 @@
+def deep_flatten_helper(lst):
+    return_val = []
+    for i in lst:
+        if hasattr(i, '__iter__'):
+            return_val += deep_flatten_helper(i)
+        else:
+            return_val += [i]
+    return return_val
+
+
+def deep_flatten(lst):
+    return iter(deep_flatten_helper(lst))
