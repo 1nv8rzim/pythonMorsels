394e1c072d6d6d9bf5774b758acbe1f84f167d1b
Maxwell <msf9542@g.rit.edu>
2020-08-18 20:44:51 -0400

    completed bonus 1 and 2

--- suppress/suppress.py
+++ suppress/suppress.py
@@ -1,1 +1,30 @@
 
+class suppress:
+
+    def __init__(self, *errors):
+        self.errors = errors
+        self.exception = None
+        self.traceback = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_type:
+            self.exception = exc_value
+            self.traceback = exc_traceback
+
+            if exc_type in self.errors:
+                return True
+            else:
+                for error in self.errors:
+                    if issubclass(exc_type, error):
+                        return True
+            return False
+        return True
+
+    def __call__(self, function):
+        try:
+            return_val = function()
+        except self.errors:
+            return return_val
