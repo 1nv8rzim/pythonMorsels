b41c6e54115154d8b209578fe910be3e17d23bf8
Maxwell <msf9542@g.rit.edu>
2020-09-28 17:34:36 -0400

    finished bonus 1

--- cd/cd.py
+++ cd/cd.py
@@ -1,13 +1,20 @@
 import os
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 
 class cd:
-    def __init__(self, subdir):
-        self.subdir = subdir
+    def __init__(self, sub_dir=None):
+        self.sub_dir = sub_dir
 
     def __enter__(self):
-        self.original_dir = os.getcwd()
-        os.chdir(self.subdir)
+        self.original_dir = Path.cwd()
+        self.temp_dir = TemporaryDirectory() if self.sub_dir is None else None
+        self.new_dir = self.temp_dir.name if self.sub_dir is None else self.original_dir.joinpath(
+            self.sub_dir)
+        os.chdir(self.new_dir)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         os.chdir(self.original_dir)
+        if self.sub_dir is None:
+            self.temp_dir.cleanup()
