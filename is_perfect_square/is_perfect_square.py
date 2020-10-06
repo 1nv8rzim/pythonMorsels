6f7fe3b0a3e2d64448a079c583ce406b2ae22a48
Maxwell <msf9542@g.rit.edu>
2020-10-05 20:37:41 -0400

    finished problem

--- is_perfect_square/is_perfect_square.py
+++ is_perfect_square/is_perfect_square.py
@@ -1,7 +1,26 @@
-from math import sqrt
+import cmath
+import math
+import numbers
+
+from decimal import Decimal, getcontext
 
 
-def is_perfect_square(num):
-    if num < 0:
+PRECISION = 100
+
+
+def is_perfect_square(x, complex_type=complex, complex=False):
+    if not isinstance(x, numbers.Number):
+        raise TypeError("{} is not a numeric value".format(x))
+    elif isinstance(x, complex_type) and complex is False:
+        raise TypeError("x cannot be complex number if 'complex' "
+                        "keyword argument is False")
+
+    if not isinstance(x, complex_type) and x < 0 and complex is False:
         return False
-    return sqrt(num).is_integer()
+    elif isinstance(x, complex_type) or x < 0:
+        root = cmath.sqrt(x)
+        return root.real.is_integer() and root.imag.is_integer()
+    else:
+        getcontext().prec = 100
+        root = Decimal(x).sqrt()
+        return root == int(root)
