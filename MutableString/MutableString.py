0178462d943bb3466923a7d28b1ab1ea61453d1d
Maxwell <msf9542@g.rit.edu>
2020-10-19 18:54:19 -0400

    base problem finsihed

--- MutableString/MutableString.py
+++ MutableString/MutableString.py
@@ -1,12 +1,12 @@
 class MutableString:
     def __init__(self, string):
-        self.string = list(string)
+        self.string = list(str(string))
 
     def __str__(self):
         return str().join(element for element in self.string)
 
     def __repr__(self):
-        return str(self)
+        return "'" + str(self) + "'"
 
     def __getitem__(self, key):
         return str().join(element for element in self.string[key])
@@ -15,16 +15,28 @@ class MutableString:
         self.string[key] = value
 
     def endswith(self, value):
-        return self.string[-1] == value
+        return str(self).endswith(value)
 
     def __add__(self, value):
-        return self.string + value
+        return MutableString(str(self) + value)
+
+    def __radd__(self, value):
+        return MutableString(value + str(self))
 
     def lower(self):
         return str(self).lower()
 
-    def __in__(self, value):
+    def upper(self):
+        return str(self).upper()
+
+    def __contains__(self, value):
         return value in str(self)
 
-    def len(self):
+    def __len__(self):
         return len(self.string)
+
+    def __eq__(self, value):
+        return value == str(self)
+
+    def replace(self, key, value):
+        return str(self).replace(key, value)
