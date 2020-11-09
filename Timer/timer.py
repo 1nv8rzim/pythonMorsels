b0620e6c67bf7da182bc0dac42d0c1679ae9cb15
Maxwell <msf9542@g.rit.edu>
2020-11-09 18:32:48 -0500

    bonus 2 completed

--- Timer/timer.py
+++ Timer/timer.py
@@ -1,9 +1,11 @@
 from time import time
+from contextlib import ContextDecorator
 
 
-class Timer:
-    def __init__(self):
+class Timer(ContextDecorator):
+    def __init__(self, function=None):
         self.runs = []
+        self.function = function
 
     @property
     def elapsed(self):
@@ -15,3 +17,9 @@ class Timer:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.runs.append(time() - self.time)
+
+    def __call__(self, *args, **kwargs):
+        self.time = time()
+        return_value = self.function(*args, **kwargs)
+        self.runs.append(time() - self.time)
+        return return_value
